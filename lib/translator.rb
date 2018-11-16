f490d3f6644ad649555aa729f6e58202b0062250
Connor Finnegan <connorfinnegan16@gmail.com>
2018-11-16 06:49:32 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   library = YAML.load_file(file_path)
   hash = {"get_meaning": {}, "get_emoticon": {}}
   library.each do |word, symbol|
-    hash["get_meaning"][]
+    hash["get_meaning"][symbol[1]]
   end
 end
 
